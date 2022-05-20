0bf3ed7e9a6d03ac7e590c8543fd4c07c018bafd
Muema <KEN20952@kcbgroup.com>
2022-05-20 14:28:15 +0300

    Initial project structure

---
+++ src/main/java/com/example/blog_springboot_restapi/BlogSpringBootRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.blog_springboot_restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogSpringBootRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogSpringBootRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/blog_springboot_restapi/BlogSpringBootRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blog_springboot_restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogSpringBootRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
