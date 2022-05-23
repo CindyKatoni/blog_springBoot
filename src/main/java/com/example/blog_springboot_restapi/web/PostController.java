c93b1a659fc22cc2706e8aa80a53c5396cdce607
Muema <KEN20952@kcbgroup.com>
2022-05-23 15:27:03 +0300

    Add getmapping handler for getting all posts and returning them in a list.

--- src/main/java/com/example/blog_springboot_restapi/web/PostController.java
+++ src/main/java/com/example/blog_springboot_restapi/web/PostController.java
@@ -4,10 +4,10 @@ import com.example.blog_springboot_restapi.payload.PostDto;
 import com.example.blog_springboot_restapi.service.PostService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.w3c.dom.stylesheets.LinkStyle;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/posts")
@@ -23,5 +23,11 @@ public class PostController {
     @PostMapping
     public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto){
         return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
+    }
+
+    //Get all posts
+    @GetMapping()
+    public List<PostDto> getAllPosts(){
+        return null;
     }
 }
