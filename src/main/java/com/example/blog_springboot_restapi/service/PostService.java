34174d4b50700dd2408962bbed42c34681d604c3
Muema <KEN20952@kcbgroup.com>
2022-05-23 15:23:46 +0300

    Create PostService interface and add createPost and getAllPosts methods.

--- src/main/java/com/example/blog_springboot_restapi/service/PostService.java
+++ src/main/java/com/example/blog_springboot_restapi/service/PostService.java
@@ -2,6 +2,10 @@ package com.example.blog_springboot_restapi.service;
 
 import com.example.blog_springboot_restapi.payload.PostDto;
 
+import java.util.List;
+
 public interface PostService {
     PostDto createPost(PostDto postDto);
+
+    List<PostDto> getAllPosts();
 }
