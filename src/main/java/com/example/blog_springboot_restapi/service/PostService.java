9dd2f33bfe3e3341d3367b5fb4ba40dfdb805706
Muema <KEN20952@kcbgroup.com>
2022-05-25 09:39:43 +0300

    create methods for CRUD.

--- src/main/java/com/example/blog_springboot_restapi/service/PostService.java
+++ src/main/java/com/example/blog_springboot_restapi/service/PostService.java
@@ -3,9 +3,15 @@ package com.example.blog_springboot_restapi.service;
 import com.example.blog_springboot_restapi.payload.PostDto;
 
 import java.util.List;
+import java.util.Map;
 
 public interface PostService {
     PostDto createPost(PostDto postDto);
 
     List<PostDto> getAllPosts();
+//    Map<String, String> getAllPosts();
+
+    PostDto getPostById(long id);
+
+    PostDto updatePost(PostDto postDto, long id);
 }
