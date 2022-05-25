8bfb789acb681f6d5bdfa9584815ebfc0afa3ac5
Muema <KEN20952@kcbgroup.com>
2022-05-25 15:53:27 +0300

    Create two private methods. one to convert dto to entity another to convert entity to dto. create comment instance, retrieve post from db using postId, set comment to post, save, return dto object.

--- src/main/java/com/example/blog_springboot_restapi/service/impl/CommentServiceImpl.java
+++ src/main/java/com/example/blog_springboot_restapi/service/impl/CommentServiceImpl.java
@@ -1,2 +1,60 @@
-package com.example.blog_springboot_restapi.service.impl;public class CommentServiceImpl {
+package com.example.blog_springboot_restapi.service.impl;
+
+import com.example.blog_springboot_restapi.exception.ResourceNotFoundException;
+import com.example.blog_springboot_restapi.model.Comment;
+import com.example.blog_springboot_restapi.model.Post;
+import com.example.blog_springboot_restapi.payload.CommentDto;
+import com.example.blog_springboot_restapi.repository.CommentRepository;
+import com.example.blog_springboot_restapi.repository.PostRepository;
+import com.example.blog_springboot_restapi.service.CommentService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommentServiceImpl implements CommentService {
+    private CommentRepository commentRepository;
+    private PostRepository postRepository;
+
+    public CommentServiceImpl(CommentRepository commentRepository, PostRepository postRepository) {
+        this.commentRepository = commentRepository;
+        this.postRepository = postRepository;
+    }
+
+    @Override
+    public CommentDto createComment(Long postId, CommentDto commentDto){
+        //Convert commentDto to Comment Entity
+        Comment comment = mapToEntity(commentDto);
+
+        //Retrieve Post Entity by id
+        Post post = postRepository.findById(postId).
+                orElseThrow(() -> new ResourceNotFoundException("Post", "id", postId));
+
+        //Set post to comment entity
+        comment.setPost(post);
+
+        //save comment entity to the db
+
+        Comment newComment = commentRepository.save(comment);
+
+        return mapToDto(newComment);
+
+    }
+
+    //convert entity to DTO
+    private CommentDto mapToDto(Comment comment){
+        CommentDto commentDto = new CommentDto();
+        commentDto.setId(comment.getId());
+        commentDto.setName(comment.getName());
+        commentDto.setEmail(comment.getEmail());
+        commentDto.setMessageBody(comment.getMessageBody());
+        return commentDto;
+    }
+    //convert DTO to entity
+    private Comment mapToEntity(CommentDto commentDto){
+        Comment comment = new Comment();
+        comment.setId(commentDto.getId());
+        comment.setName(commentDto.getName());
+        comment.setEmail(commentDto.getEmail());
+        comment.setMessageBody(commentDto.getMessageBody());
+        return comment;
+    }
 }
