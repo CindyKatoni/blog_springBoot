2453eedf7b1f797a27ef4297574dc2c54c753f06
Muema <KEN20952@kcbgroup.com>
2022-05-25 12:42:41 +0300

    Create comment JPA entity.

---
+++ src/main/java/com/example/blog_springboot_restapi/model/Comment.java
@@ -0,0 +1,29 @@
+package com.example.blog_springboot_restapi.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "comments" )
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+
+    private String name;
+    private String email;
+    private String messageBody;
+
+    //establish one-to-many relationship
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)//specify FK
+    private Post post;
+
+}
