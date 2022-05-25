9243ec655032a6ffae16ce006cbc36fc99ce4d74
Muema <KEN20952@kcbgroup.com>
2022-05-25 10:39:06 +0300

    Use PostResponse as data type in PostService interface.

--- src/main/java/com/example/blog_springboot_restapi/service/PostService.java
+++ src/main/java/com/example/blog_springboot_restapi/service/PostService.java
@@ -1,6 +1,7 @@
 package com.example.blog_springboot_restapi.service;
 
 import com.example.blog_springboot_restapi.payload.PostDto;
+import com.example.blog_springboot_restapi.payload.PostResponse;
 
 import java.util.List;
 import java.util.Map;
@@ -8,7 +9,7 @@ import java.util.Map;
 public interface PostService {
     PostDto createPost(PostDto postDto);
 
-    List<PostDto> getAllPosts(int pageNo, int pageSize);
+    PostResponse getAllPosts(int pageNo, int pageSize);
 //    Map<String, String> getAllPosts();
 
     PostDto getPostById(long id);
