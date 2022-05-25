99938861183bf3cd765fb58bd159e33ba97ac442
Muema <KEN20952@kcbgroup.com>
2022-05-25 10:50:58 +0300

    Add sortBy param to PostServiceImpl, add sortBy object to Sort class in Pageable instance.

--- src/main/java/com/example/blog_springboot_restapi/service/impl/PostServiceImpl.java
+++ src/main/java/com/example/blog_springboot_restapi/service/impl/PostServiceImpl.java
@@ -9,6 +9,7 @@ import com.example.blog_springboot_restapi.payload.PostDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
@@ -39,10 +40,10 @@ public class PostServiceImpl implements PostService {
     }
 
     @Override
-    public PostResponse getAllPosts(int pageNo, int pageSize) {
+    public PostResponse getAllPosts(int pageNo, int pageSize, String sortBy) {
 
         //Create pageable instance
-        Pageable pageable = PageRequest.of(pageNo, pageSize);
+        Pageable pageable = PageRequest.of(pageNo, pageSize, Sort.by(sortBy));
 
         Page<Post> posts = postRepository.findAll(pageable);
 
