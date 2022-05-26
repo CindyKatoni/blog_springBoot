128a6da07fd52c22a5e487bd55175749786ee838
Muema <KEN20952@kcbgroup.com>
2022-05-26 11:01:45 +0300

    Add findPostById method to the repository.

--- src/main/java/com/example/blog_springboot_restapi/repository/CommentRepository.java
+++ src/main/java/com/example/blog_springboot_restapi/repository/CommentRepository.java
@@ -3,6 +3,8 @@ package com.example.blog_springboot_restapi.repository;
 import com.example.blog_springboot_restapi.model.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CommentRepository extends JpaRepository<Comment, Long> {
+import java.util.List;
 
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByPostId(long postId);
 }
