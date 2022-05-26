ed2b164f8ceafb7b6aa5a750ca610683777789f1
Muema <KEN20952@kcbgroup.com>
2022-05-26 11:05:08 +0300

    Edit request mapping url.

--- src/main/java/com/example/blog_springboot_restapi/web/CommentController.java
+++ src/main/java/com/example/blog_springboot_restapi/web/CommentController.java
@@ -1,2 +1,27 @@
-package com.example.blog_springboot_restapi.web;public class CommentController {
+package com.example.blog_springboot_restapi.web;
+
+import com.example.blog_springboot_restapi.payload.CommentDto;
+import com.example.blog_springboot_restapi.service.CommentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.lang.*;
+
+@RestController
+@RequestMapping("/api/")
+public class CommentController {
+
+    private CommentService commentService;
+
+    public CommentController(CommentService commentService) {
+        this.commentService = commentService;
+    }
+
+    @PostMapping("/posts/{postId}/comments")
+    public ResponseEntity<CommentDto> createComment(@PathVariable(value = "postId") long postId,
+                                                    @RequestBody CommentDto commentDto){
+        return new ResponseEntity<>(commentService.createComment(postId, commentDto), HttpStatus.CREATED);
+    }
+
+
 }
