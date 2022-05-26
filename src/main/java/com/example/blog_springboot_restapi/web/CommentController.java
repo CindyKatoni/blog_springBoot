ecd7f91668d129ee775fdd5e3ebc38c2e83faf4e
Muema <KEN20952@kcbgroup.com>
2022-05-26 11:38:59 +0300

    create endpoint for getting by comments using post id

--- src/main/java/com/example/blog_springboot_restapi/web/CommentController.java
+++ src/main/java/com/example/blog_springboot_restapi/web/CommentController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.lang.*;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/")
@@ -18,9 +19,14 @@ public class CommentController {
     }
 
     @PostMapping("/posts/{postId}/comments")
-    public ResponseEntity<CommentDto> createComment(@PathVariable(value = "postId") long postId,
+    public ResponseEntity<CommentDto> createComment(@PathVariable(value = "postId") Long postId,
                                                     @RequestBody CommentDto commentDto){
         return new ResponseEntity<>(commentService.createComment(postId, commentDto), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/posts/{postId}/comments")
+    public List<CommentDto> getCommentsByPostId(@PathVariable(value = "postId") Long postId) {
+        return commentService.getCommentsByPostId(postId);
     }
 
 
