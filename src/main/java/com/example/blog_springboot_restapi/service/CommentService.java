9b2c438f69c0b6b30c60f2b8cdf860639d166250
Muema <KEN20952@kcbgroup.com>
2022-05-26 11:03:06 +0300

    Register method to get all comments by post id.

--- src/main/java/com/example/blog_springboot_restapi/service/CommentService.java
+++ src/main/java/com/example/blog_springboot_restapi/service/CommentService.java
@@ -1,8 +1,13 @@
 package com.example.blog_springboot_restapi.service;
 
+import java.lang.*;
+import java.util.List;
+
 import com.example.blog_springboot_restapi.payload.CommentDto;
 
 public interface CommentService {
 
-    CommentDto createComment(CommentDto commentDto);
+    CommentDto createComment(long postId, CommentDto commentDto);
+
+    List<CommentDto> getCommentsByPostId(long postId);
 }
