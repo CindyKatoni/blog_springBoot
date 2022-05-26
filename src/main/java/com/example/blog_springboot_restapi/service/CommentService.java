d76319e60cdb59ec13a8638abc86e5b9d2b30405
Muema <KEN20952@kcbgroup.com>
2022-05-26 13:00:02 +0300

    Register getCommentById and updateComment methods with two arguements.

--- src/main/java/com/example/blog_springboot_restapi/service/CommentService.java
+++ src/main/java/com/example/blog_springboot_restapi/service/CommentService.java
@@ -3,13 +3,16 @@ package com.example.blog_springboot_restapi.service;
 import java.lang.*;
 import java.util.List;
 
+import com.example.blog_springboot_restapi.model.Comment;
 import com.example.blog_springboot_restapi.payload.CommentDto;
 
 public interface CommentService {
 
-    CommentDto createComment(long postId, CommentDto commentDto);
+    CommentDto createComment(Long postId, CommentDto commentDto);
 
-    List<CommentDto> getCommentsByPostId(long postId);
+    List<CommentDto> getCommentsByPostId(Long postId);
 
+    CommentDto getCommentById(Long postId, Long commentId);
 
+    CommentDto updateComment(Long postId, Long commentId);
 }
