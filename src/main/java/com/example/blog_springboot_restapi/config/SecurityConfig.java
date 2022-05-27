820dd5268a6c48ec8684279dfff57bc2c7b2e3c8
Muema <KEN20952@kcbgroup.com>
2022-05-27 17:10:04 +0300

    Create two users using UserDetailsService class. One with user role another with admin role. Use InMemoryUserDetailsManager to save it inmemory object. Permit all to access get endpoints using antMatchers.

--- src/main/java/com/example/blog_springboot_restapi/config/SecurityConfig.java
+++ src/main/java/com/example/blog_springboot_restapi/config/SecurityConfig.java
@@ -1,2 +1,39 @@
-package com.example.blog_springboot_restapi.config;public class SecurityConfig {
+package com.example.blog_springboot_restapi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/api/**").permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+
+    }
+
+    @Override
+    protected UserDetailsService userDetailsService() {
+        UserDetails ramesh = User.builder().username("ramesh").password("ramesh123").roles("USER").build();
+        UserDetails admin = User.builder().username("admin").password("admin123").roles("ADMIN").build();
+
+        return new InMemoryUserDetailsManager(ramesh, admin);
+    }
 }
