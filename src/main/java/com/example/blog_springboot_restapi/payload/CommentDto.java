1987194042c5f5bfd2aa3c72f0d05372f5f02afc
Muema <KEN20952@kcbgroup.com>
2022-05-27 14:43:41 +0300

    Use validation annotations in Post and Comment dtos.

--- src/main/java/com/example/blog_springboot_restapi/payload/CommentDto.java
+++ src/main/java/com/example/blog_springboot_restapi/payload/CommentDto.java
@@ -2,10 +2,21 @@ package com.example.blog_springboot_restapi.payload;
 
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Data
 public class CommentDto {
     private Long Id;
+
+    @NotEmpty(message = "Name should not be empty")
     private String name;
+    @NotEmpty(message = "Email should not be empty")
+    @Email(message = "Enter correct email")
     private String email;
+    @NotEmpty
+    @Size(min = 10, message = "Body should contain more than 10 characters")
     private String body;
 }
